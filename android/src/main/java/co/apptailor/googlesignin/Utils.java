a4463a4a0a8de880feccfaeb07e33029ccf6e09e
Jano <janliberacki@gmail.com>
2020-02-17 11:55:16 +0100

    Revert "migrate to AndroidX (#693)"
    This reverts commit 0825b8795ad7e241656be76e23dfdc26fa71929d.

--- android/src/main/java/co/apptailor/googlesignin/Utils.java
+++ android/src/main/java/co/apptailor/googlesignin/Utils.java
@@ -1,7 +1,7 @@
 package co.apptailor.googlesignin;
 
 import android.net.Uri;
-import androidx.annotation.NonNull;
+import android.support.annotation.NonNull;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableArray;
